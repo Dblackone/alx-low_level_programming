d61d89dc405bf038a6082eebdacc2423a8130a1b
Vollmann Akarakiri <vollmannakarakiri0@gmail.com>
2023-07-17 17:14:03 -0700

    added 3, 4

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main
+ *
+ * Return 0 always (sucess)
+ */
+int main(void)
+{
+    char letter;
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    putchar('\n');
+    }
+    return 0;
+}
