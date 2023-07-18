484f2ed0c17e391e933f15f712f06923a97e146b
Vollmann Akarakiri <vollmannakarakiri0@gmail.com>
2023-07-17 19:11:09 -0700

    5,6, and 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,7 +8,8 @@
 int main(void)
 {
 	char letters;
-	for (letters = 'z'; letters >= 'a'; letters++)
+
+	for (letters = 'z'; letters >= 'a'; letters--)
 	{
 		putchar(letters);
 	}
